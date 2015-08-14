3170f025eea3e297e4f19038982584b9a1603415
Isaac Murchie <isaac@saucelabs.com>
2015-08-14 15:24:29 -0700

    Add performance logs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import IOSLog from './lib/ios-log';
 import IOSCrashLog from './lib/ios-crash-log';
+import IOSPerformanceLog from './lib/ios-performance-log';
 
-export { IOSLog, IOSCrashLog };
+export { IOSLog, IOSCrashLog, IOSPerformanceLog };
 
